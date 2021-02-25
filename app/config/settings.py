c80650ebf1f7a7113dfd5e5ba945eb8cb4a49ef4
ingyeomental <ingyeomental@gmail.com>
2021-02-25 11:32:00 +0900

    todo app fixed

--- app/config/settings.py
+++ app/config/settings.py
@@ -46,16 +46,16 @@ INSTALLED_APPS = [
 
 # rest_framwork
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-    ],
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'knox.auth.TokenAuthentication',
-    ],
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    # ],
+    # 'DEFAULT_RENDERER_CLASSES': [
+    #     'rest_framework.renderers.JSONRenderer',
+    #     'rest_framework.renderers.BrowsableAPIRenderer',
+    # ],
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'knox.auth.TokenAuthentication',
+    # ],
     # 'DEFAULT_FILTER_BACKENDS': [
     #     'rest_framework.filters.SearchFilter',
     #     'rest_framework.filters.OrderingFilter',
